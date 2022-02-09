c1e177c8884982f5ffdff55e5f6517b7c0b2b453
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2022-02-09 13:52:33 +0000

    Added square component

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import "./styles.css";
 
-function Square() {
-  return <></>;
+// type Board = ("O" | "X" | null)[];
+
+interface Props {
+  varient: "O" | "X" | null;
+  squareIndex: number;
+  makeAMove: (squareIndex: number) => void;
+}
+
+function Square(props: Props) {
+  const { varient, squareIndex, makeAMove } = props;
+
+  function handleClick(e: ChangeEvent<HTMLDivElement>) {
+    e.preventDefault();
+    makeAMove(squareIndex);
+  }
+
+  return <div onClick={() => handleClick}>{varient}</div>;
 }
 
 export default Square;
